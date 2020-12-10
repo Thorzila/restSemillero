d00014c3c86d05f14fb5389128b57f4cd36084c3
Juan Manzano <juancamilo106055@gmail.com>
2020-12-10 09:36:04 -0500

    Ejercicio 1 prueba

--- src/main/java/com/clearminds/jcmv/servicios/RestEstudiantes.java
+++ src/main/java/com/clearminds/jcmv/servicios/RestEstudiantes.java
@@ -2,39 +2,37 @@ package com.clearminds.jcmv.servicios;
 
 import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.core.MediaType;
-
 import com.clearminds.jcmv.dtos.Estudiante;
 
 @Path("/estudiantes")
 public class RestEstudiantes {
-	
+
 	@Path("/insertar")
 	@POST
 	@Consumes(MediaType.APPLICATION_JSON)
-	public void insertar(Estudiante estudiante){
+	public void insertar(Estudiante estudiante) {
 		try {
+			System.out.println("Insertar: " + estudiante);
 			ServicioEstudiante srvestudiante = new ServicioEstudiante();
 			srvestudiante.insertarEstudiante(estudiante);
-			System.out.println("Insertar: " + estudiante);			
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
 	}
-	
+
 	@Path("/actualizar")
-	@POST
+	@PUT
 	@Consumes(MediaType.APPLICATION_JSON)
-	public void actualizar(Estudiante estudiante){
+	public void actualizar(Estudiante estudiante) {
 		try {
+			System.out.println("Actualizando: " + estudiante);
 			ServicioEstudiante srvestudiante = new ServicioEstudiante();
 			srvestudiante.actualizarEstudiante(estudiante);
-			System.out.println("Actualizando: " + estudiante);			
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
 	}
 }
